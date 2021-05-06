f91df225815d10ec51fcf4e02effb46217ea8f77
Roshan R <roshanrajeev2001@gmail.com>
2021-05-06 12:30:10 +0530

    Ellam set ayitund heppy alle?

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
         responsive: [{
             breakpoint: 768,
             settings: {
-                slidesToShow: 4
+                slidesToShow: 2
             }
         }, {
             breakpoint: 520,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 1
             }
         }]
     });
